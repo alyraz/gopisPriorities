71ca8d151eea0b236f174f3d75934144f9fadca5
Alyssa Ravasio <alyssaravasio@gmail.com>
2013-04-11 12:20:51 -0700

    create fake user and add js to create sliders
    * create fake user so html will render
    * js for home_screen to attach sliders, listen for change, and posts to checkins (still need to build this route)

---
+++ app/assets/javascripts/home_screen.js
@@ -0,0 +1,36 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+//You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(document).ready(function(){
+    var priorities = {
+        sleep:      0,
+        nutrition:  0,
+        exercise:   0,
+        meditation: 0,
+        love:       0,
+        stuff:      0,
+        work:       0,
+        passion:    0,
+        growth:     0,
+        service:    0
+    }
+    //attaches a jQuery UI slider to each div with .slider class
+    $(".slider").slider();
+    //when slider changes, set appropriate attribute of prioritities obj to slider value
+    $(".slider").slider( {
+        change: function( event, ui ) {
+            var id = $(this).attr('id');
+            priorities[id] = ui.value
+        }
+    });
+    $("#checkin-form").submit(function(e){
+        e.preventDefault();
+        $.post('/checkins', priorities).done(function(checkin_id){
+           window.location = "http://localhost:9292/checkins/" + checkin_id
+         });
+
+    });
+});
+
+
--- app/helpers/home_screen_helper.rb
+++ app/helpers/home_screen_helper.rb
@@ -1,2 +1,23 @@
 module HomeScreenHelper
+
+  def current_user
+    FakeCurrentUser.new
+  end
+
+  class FakeCurrentUser
+
+    def name
+      "Bob"
+    end
+
+    def id
+      8
+    end
+
+    def photo_src
+      "http://www.foreverboho.com/wp-content/gallery/misturadas/bob-marley.jpg"
+    end
+
+  end
+
 end
