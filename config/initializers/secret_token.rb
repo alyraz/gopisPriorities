59ccddc1bd6586a0c72027f5d0497b3770e58f50
Alyssa Ravasio <alyssaravasio@gmail.com>
2013-04-11 11:05:18 -0700

    and so it begins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GopisPriorities::Application.config.secret_token = 'd1d2963f063f87f6ae0027553f8861aa1642f2f784e81b8acf01d23d11b5bb31a22606b15409453c7caf58fec46c55b6c27732dd18e4c2de95c05bbdfbda7a86'
