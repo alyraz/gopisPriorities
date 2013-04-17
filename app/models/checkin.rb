bae3af19cc717cd91818acf5fedd995962cd400e
Apprentice <apprentice@dbc04.local>
2013-04-16 20:46:18 -0700

    add associations

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,3 +1,4 @@
 class Checkin < ActiveRecord::Base
   attr_accessible :user_id, :sleep, :nutrition, :exercise, :meditation, :love, :stuff, :work, :passion, :growth, :service
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :photo_src, :google_id
+  has_many :checkins
 end
