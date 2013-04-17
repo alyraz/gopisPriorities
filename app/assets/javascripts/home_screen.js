09d9e484fca2fccc2d72a195af8f2a89d59f4041
Apprentice <apprentice@dbc04.local>
2013-04-16 20:25:04 -0700

    Refactoring of the templates to use Checkings.attrs

--- app/assets/javascripts/home_screen.js
+++ app/assets/javascripts/home_screen.js
@@ -3,18 +3,6 @@
 //You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 $(document).ready(function(){
-    var priorities = {
-        sleep:      0,
-        nutrition:  0,
-        exercise:   0,
-        meditation: 0,
-        love:       0,
-        stuff:      0,
-        work:       0,
-        passion:    0,
-        growth:     0,
-        service:    0
-    }
     //attaches a jQuery UI slider to each div with .slider class
     $(".slider").slider();
     //when slider changes, set appropriate attribute of prioritities obj to slider value
--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,6 +1,7 @@
 class CheckinsController < ApplicationController
 
   def new
+    @priorities = Checkin.attribute_names.reject{|i| ['id', 'created_at', 'updated_at', 'user_id'].include?(i)}
   end
 
 end
